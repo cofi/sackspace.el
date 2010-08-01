1261a5a06126b921bf1022008c306b8a1ba0602e
Michael Markert <markert.michael@gmail.com>
2010-08-01 18:11:23 +0200

    Added Issue-Tracker link.
    Found bug, that's why.

--- sackspace.el
+++ sackspace.el
@@ -24,6 +24,9 @@
 ;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
+;; Known Bugs:
+;; See http://github.com/cofi/sackspace.el/issues
+
 ;; Homepage: http://github.com/cofi/sackspace.el
 ;; Git-Repository: git://github.com/cofi/sackspace.el.git
 
