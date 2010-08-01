b0b10b97624fb01f0aac1c286651e246b6e9974c
Michael Markert <markert.michael@gmail.com>
2010-08-01 16:30:42 +0200

    Added legalese.

---
+++ sackspace.el
@@ -0,0 +1,25 @@
+;; sackspace.el --- a better backspace
+
+;; Copyright (C) 2010 by Michael Markert
+;; Author: 2010 Michael Markert <markert.michael@googlemail.com>
+;; Created: 2010/08/01
+;; Version: 0.0
+;; Last modified: 2010-08-01 16:29:36 +0200
+;; Keywords: delete
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
